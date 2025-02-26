00ae2a6510654028fd0d898314b1e62d7397b524
babatundehabeeb2@gmail.com <babatundehabeeb2@gmail.com>
2025-02-26 09:37:34 +0000

    update fct_fhv_p90

---
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_fhv_monthly_zone_traveltime_p90.sql
@@ -0,0 +1,37 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with fhv_trips as (
+    select
+        fhv_trip.dispatching_base_num, 
+        fhv_trip.service_type,
+        fhv_trip.pickup_locationid,
+        pickup_zone.zone as pickup_zone,
+        fhv_trip.dropoff_locationid,
+        dropoff_zone.zone as dropoff_zone,
+        fhv_trip.pickup_datetime, 
+        fhv_trip.dropoff_datetime, 
+        fhv_trip.sr_flag,
+        fhv_trip.year,
+        fhv_trip.month,
+        -- Calculate trip_duration in seconds
+        timestamp_diff(fhv_trip.dropoff_datetime, fhv_trip.pickup_datetime, 'second') as trip_duration
+    from {{ ref('fact_fhv_trips') }} as fhv_trip
+    inner join {{ ref('dim_zones') }} as pickup_zone
+        on fhv_trip.pickup_locationid = pickup_zone.locationid
+    inner join {{ ref('dim_zones') }} as dropoff_zone
+        on fhv_trip.dropoff_locationid = dropoff_zone.locationid
+)
+
+select
+    year,
+    month,
+    pickup_zone,
+    dropoff_zone,
+    -- Calculate the 90th percentile of trip_duration for each group
+    percentile_cont(0.9) within group (order by trip_duration) as trip_duration_p90
+from fhv_trips
+group by year, month, pickup_zone, dropoff_zone
